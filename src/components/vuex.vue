69b2467dbc3358b2d7be6e9c270a2f658f0e64f7
lee <shengminfang@foxmail.com>
2020-03-16 16:07:31 +0800

    星星

--- src/components/vuex.vue
+++ src/components/vuex.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="vuex">{{test}}</div>
+  <div class="vuex">
+    <div class="star" v-for="(item, index) in test" :key="index">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,6 +17,51 @@ export default {
 
 <style lang="scss">
   .vuex {
-    font-size: 500px;
+    // font-size: 500px;
+    .star{
+        width:0px;height:0px;
+        border-bottom:70px solid rgba(250,250,0,1);
+        border-left:100px solid transparent;
+        border-right:100px solid transparent;
+        margin:100px auto;
+        /*到这一步为止，得到的是一个向上的等腰三角形*/
+        -webkit-transform: rotate(35deg);
+        -ms-transform: rotate(35deg);
+        -o-transform: rotate(35deg);
+        transform: rotate(35deg);
+        /*顺时针旋转35deg*/
+        position:relative;
+        &:before {
+          content:"";
+        width:0px;
+        height:0px;
+        border-bottom:70px solid rgba(250,250,0,1);
+        border-left:30px solid transparent;
+        border-right:30px solid transparent;
+        /*伪类所添加的内容继承原来的样式也发生了顺时针旋转*/
+        -webkit-transform: rotate(-35deg);
+        -ms-transform: rotate(-35deg);
+        -o-transform: rotate(-35deg);
+        transform: rotate(-35deg);
+        /*设置负值，就会逆时针旋转*/
+        position:absolute;
+        top:-45px;left:-65px;
+        /*定位调整位置*/
+        }
+        &:after {
+          content:"";
+          width:0px;
+          height:0px;
+          position:absolute;
+          border-bottom:70px solid rgba(250,250,0,1);
+          border-left:100px solid transparent;
+          border-right:100px solid transparent;
+          -webkit-transform: rotate(-70deg);
+          -ms-transform: rotate(-70deg);
+          -o-transform: rotate(-70deg);
+          transform: rotate(-70deg);
+          top:3px;left:-105px;
+        }
+    }
   }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    test: 0
+    test: 1
   },
   getters: {
     title: state => state.test
